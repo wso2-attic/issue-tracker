261b0e62fdd794d44cf3c832d77f3d0490dd2495
manisha <manisha@wso2.com>
2013-08-12 14:17:25 +0530

    issue tracker BE

---
+++ org.wso2.carbon.issue.tracker/src/main/java/org/wso2/carbon/issue/tracker/bean/Comment.java
@@ -0,0 +1,86 @@
+/*
+ *  Copyright (c) 2005-2013, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2.carbon.issue.tracker.bean;
+
+public class Comment {
+
+    private int commentId;
+    private String commentDescription;
+    private String commentCreatedTime;
+    private String commentUpdatedTime;
+    private String commentAuthor;
+    private int issueId;
+    private int userId;
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getCommentDescription() {
+        return commentDescription;
+    }
+
+    public void setCommentDescription(String commentDescription) {
+        this.commentDescription = commentDescription;
+    }
+
+    public String getCommentCreatedTime() {
+        return commentCreatedTime;
+    }
+
+    public void setCommentCreatedTime(String commentCreatedTime) {
+        this.commentCreatedTime = commentCreatedTime;
+    }
+
+    public String getCommentUpdatedTime() {
+        return commentUpdatedTime;
+    }
+
+    public void setCommentUpdatedTime(String commentUpdatedTime) {
+        this.commentUpdatedTime = commentUpdatedTime;
+    }
+
+    public String getCommentAuthor() {
+        return commentAuthor;
+    }
+
+    public void setCommentAuthor(String commentAuthor) {
+        this.commentAuthor = commentAuthor;
+    }
+
+    public int getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(int issueId) {
+        this.issueId = issueId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
---
+++ org.wso2.carbon.issue.tracker/src/main/java/org/wso2/carbon/issue/tracker/bean/Issue.java
@@ -0,0 +1,131 @@
+/*
+ *  Copyright (c) 2005-2013, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2.carbon.issue.tracker.bean;
+
+public class Issue {
+
+    private int issueId;
+    private String issueTitle;
+    private String issueDescription;
+    private String issueType;
+    private String issuePriority;
+    private String issueOwner;
+    private String issueStatus;
+    private String issueAssignee;
+    private String projectVersion;
+    private String issueCreatedTime;
+    private String issueUpdatedTime;
+    private String issueSeverity;
+
+    public int getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(int issueId) {
+        this.issueId = issueId;
+    }
+
+    public String getIssueTitle() {
+        return issueTitle;
+    }
+
+    public void setIssueTitle(String issueTitle) {
+        this.issueTitle = issueTitle;
+    }
+
+    public String getIssueDescription() {
+        return issueDescription;
+    }
+
+    public void setIssueDescription(String issueDescription) {
+        this.issueDescription = issueDescription;
+    }
+
+    public String getIssueType() {
+        return issueType;
+    }
+
+    public void setIssueType(String issueType) {
+        this.issueType = issueType;
+    }
+
+    public String getIssuePriority() {
+        return issuePriority;
+    }
+
+    public void setIssuePriority(String issuePriority) {
+        this.issuePriority = issuePriority;
+    }
+
+    public String getIssueOwner() {
+        return issueOwner;
+    }
+
+    public void setIssueOwner(String issueOwner) {
+        this.issueOwner = issueOwner;
+    }
+
+    public String getIssueStatus() {
+        return issueStatus;
+    }
+
+    public void setIssueStatus(String issueStatus) {
+        this.issueStatus = issueStatus;
+    }
+
+    public String getIssueAssignee() {
+        return issueAssignee;
+    }
+
+    public void setIssueAssignee(String issueAssignee) {
+        this.issueAssignee = issueAssignee;
+    }
+
+    public String getProjectVersion() {
+        return projectVersion;
+    }
+
+    public void setProjectVersion(String projectVersion) {
+        this.projectVersion = projectVersion;
+    }
+
+    public String getIssueCreatedTime() {
+        return issueCreatedTime;
+    }
+
+    public void setIssueCreatedTime(String issueCreatedTime) {
+        this.issueCreatedTime = issueCreatedTime;
+    }
+
+    public String getIssueUpdatedTime() {
+        return issueUpdatedTime;
+    }
+
+    public void setIssueUpdatedTime(String issueUpdatedTime) {
+        this.issueUpdatedTime = issueUpdatedTime;
+    }
+
+    public String getIssueSeverity() {
+        return issueSeverity;
+    }
+
+    public void setIssueSeverity(String issueSeverity) {
+        this.issueSeverity = issueSeverity;
+    }
+}
