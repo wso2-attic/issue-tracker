5c8e9cbb34bf76d2b703439a94edb1aea23bc7cb
manisha <manisha@wso2.com>
2013-08-12 15:37:44 +0530

    DBConfiguration class changes

--- org.wso2.carbon.issue.tracker/src/main/java/org/wso2/carbon/issue/tracker/util/DBConfiguration.java
+++ org.wso2.carbon.issue.tracker/src/main/java/org/wso2/carbon/issue/tracker/util/DBConfiguration.java
@@ -64,56 +64,5 @@ public class DBConfiguration {
         this.driverName = driverName;
     }
 
-    public void connectToDatabase() {
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            //STEP 2: Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
 
-            //STEP 3: Open a connection
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection(Constants.DB_URL, "root", "root");
-            System.out.println("Connected database successfully...");
-
-            //STEP 4: Execute a query
-            System.out.println("Inserting records into the table...");
-            stmt = conn.createStatement();
-
-            String sql = "INSERT INTO Registration " +
-                    "VALUES (100, 'Zara', 'Ali', 18)";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Registration " +
-                    "VALUES (101, 'Mahnaz', 'Fatma', 25)";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Registration " +
-                    "VALUES (102, 'Zaid', 'Khan', 30)";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Registration " +
-                    "VALUES(103, 'Sumit', 'Mittal', 28)";
-            stmt.executeUpdate(sql);
-            System.out.println("Inserted records into the table...");
-
-        } catch (SQLException se) {
-            //Handle errors for JDBC
-            se.printStackTrace();
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null)
-                    conn.close();
-            } catch (SQLException se) {
-            }// do nothing
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
-    }
 }
