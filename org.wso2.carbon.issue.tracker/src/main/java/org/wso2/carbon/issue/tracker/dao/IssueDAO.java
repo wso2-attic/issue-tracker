1c72b0536b3cc40d68d7dd84a202478f836ee595
asanka88 <asankanuwanbandara@gmail.com>
2013-08-14 12:27:17 +0530

    changing issuedao

--- org.wso2.carbon.issue.tracker/src/main/java/org/wso2/carbon/issue/tracker/dao/IssueDAO.java
+++ org.wso2.carbon.issue.tracker/src/main/java/org/wso2/carbon/issue/tracker/dao/IssueDAO.java
@@ -29,43 +29,75 @@ import org.wso2.carbon.issue.tracker.bean.Issue;
 import org.wso2.carbon.issue.tracker.util.DBConfiguration;
 
 public class IssueDAO {
-	Log log=LogFactory.getLog(IssueDAO.class);
-	Connection dbConnection=null;
+	Log log = LogFactory.getLog(IssueDAO.class);
+	Connection dbConnection = null;
+
 	public IssueDAO() {
-		dbConnection=DBConfiguration.getDBConnection();
+		dbConnection = DBConfiguration.getDBConnection();
 		log.debug("database connection successfully retrieved");
 	}
-	public void createIssue(Issue issue) throws SQLException{
-		PreparedStatement st = (PreparedStatement) dbConnection
-				.prepareStatement("INSERT INTO Issues VALUES(?,?,?,?,?,?,?,?,?,?,?,?)");
-		st.setString(1,Integer.toString(issue.getId()));
-		st.setString(2,issue.getSummary());
-		st.setString(3,issue.getDescription());
-		st.setString(4,issue.getType());
-		st.setString(5,issue.getPriority());
-		st.setString(6,issue.getOwner());
-		st.setString(7,issue.getStatus());
-		st.setString(8,issue.getAssignee());
-		st.setString(9,issue.getVersion());
-		st.setString(10,issue.getCreatedTime());
-		st.setString(11,issue.getUpdatedTime());
-		st.setString(12,issue.getSeverity());
-		st.executeUpdate();
-	}
 
-	public void updateIssueStatus(Issue issue, String status){
+	public void createIssue(Issue issue) {
 
-	}
-
-	public void updateWithComment(Issue issue, Comment comment){
-
-	}
-
-	public void viewIssue(Issue issue){
+		try {
+			PreparedStatement st = (PreparedStatement) dbConnection
+					.prepareStatement("INSERT INTO Issues VALUES(?,?,?,?,?,?,?,?,?,?,?,?)");
+			st.setString(1, Integer.toString(issue.getId()));
+			st.setString(2, issue.getSummary());
+			st.setString(3, issue.getDescription());
+			st.setString(4, issue.getType());
+			st.setString(5, issue.getPriority());
+			st.setString(6, issue.getOwner());
+			st.setString(7, issue.getStatus());
+			st.setString(8, issue.getAssignee());
+			st.setString(9, issue.getVersion());
+			st.setString(10, issue.getCreatedTime());
+			st.setString(11, issue.getUpdatedTime());
+			st.setString(12, issue.getSeverity());
+			st.executeUpdate();
+		} catch (SQLException e) {
+			log.info("Erro occure while creating the issue " + issue.getId()
+					+ " " + e.getMessage());
+		}
 
 	}
 
-	public void viewAllIssues(){
+	/**
+	 * Any column of the issue can be updated using this method. REMEBER!! This
+	 * is to use only one attribute of the issue is changed
+	 * 
+	 * @param issue
+	 *            issue to be changed
+	 * @param columnName
+	 *            column name
+	 * @param value
+	 *            new value
+	 */
+	public void updateIssueAttribute(Issue issue, String columnName,
+			String value) {
+		try {
+			PreparedStatement st = (PreparedStatement) dbConnection
+					.prepareStatement("UPDATE Issues SET ?=? WHERE id=?");
+			st.setString(1, columnName);
+			st.setString(2, value);
+			st.setString(3, Integer.toString(issue.getId()));
+			st.executeUpdate();
+		} catch (SQLException e) {
+			log.info("Erro occure while creating the issue " + issue.getId()
+					+ " " + e.getMessage());
+		}
+	}
+
+	public void updateWithComment(Issue issue, Comment comment) {
+		
+
+	}
+
+	public void viewIssue(Issue issue) {
+
+	}
+
+	public void viewAllIssues() {
 
 	}
 }
