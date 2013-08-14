b5cc0d1b89bd41b5b9a0227ab34df362f97e35c7
Nihanth <nihanthjoseph@gmail.com>
2013-08-14 14:41:36 +0530

    folder structure is changed

--- org.wso2.carbon.issue.tracker/src/main/java/org/wso2/carbon/issue/tracker/server/impl/CommentServiceImpl.java
+++ org.wso2.carbon.issue.tracker/src/main/java/org/wso2/carbon/issue/tracker/server/impl/CommentServiceImpl.java
@@ -1,55 +1,43 @@
-package org.wso2.carbon.issue.tracker.internal;
+package org.wso2.carbon.issue.tracker.server.impl;
 
 import org.wso2.carbon.issue.tracker.bean.Comment;
 import org.wso2.carbon.issue.tracker.dao.CommentDAO;
 import org.wso2.carbon.issue.tracker.dao.impl.CommentDAOImpl;
+import org.wso2.carbon.issue.tracker.server.CommentService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
  * User: nihanth
- * Date: 8/13/13
- * Time: 11:15 AM
+ * Date: 8/14/13
+ * Time: 1:49 PM
  * To change this template use File | Settings | File Templates.
  */
-@Path("/commentservice")
-public class CommentService {
-    @GET
-    @Path("/issue/{issueId}/")
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+public class CommentServiceImpl implements CommentService {
+
     public List<Comment> getCommentsForIssue(@PathParam("issueId") int issueId) throws Exception {
         CommentDAO commentDAO = new CommentDAOImpl();
         List<Comment> comments = commentDAO.getCommentsForIssue(issueId);
-        // return Response.status(200).entity("addUser is called, userAgent : " + userAgent).build();
-        System.out.println(">>>>>>" + comments.size());
-        for(Comment c:comments){
-            System.out.println(c.getCommentId());
-        }
 
         GenericEntity<List<Comment>> entity = new GenericEntity<List<Comment>>(comments){} ;
         return entity.getEntity();
     }
 
-    @POST
-    @Path("/postcomment")
-    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public String postCommentForIssue(Comment comment) throws Exception{
+    public Response postCommentForIssue(Comment comment) throws Exception{
         System.out.println("comment " + comment.getComment());
-        //CommentDAO commentDAO = new CommentDAOImpl();
-        //commentDAO.postCommentForIssue(comment);
-        return null;
+        System.out.println("issue id: " + comment.getIssueId());
+
+        CommentDAO commentDAO = new CommentDAOImpl();
+        commentDAO.postCommentForIssue(comment);
+
+        return Response.ok().entity(comment).type(MediaType.APPLICATION_JSON).build();
     }
 
-
-
-    @DELETE
-    @Path("/deletecomment")
-    @Produces(MediaType.APPLICATION_JSON)
     public String deleteComment(@FormParam("usreId") int userId,
                                 @FormParam("commentId") int commentId) throws Exception {
         CommentDAO commentDAO = new CommentDAOImpl();
@@ -58,9 +46,6 @@ public class CommentService {
         return null;
     }
 
-    @POST
-    @Path("/editcomment")
-    @Consumes(MediaType.APPLICATION_JSON)
     public String editComment(Comment comment) throws Exception {
         CommentDAO commentDAO = new CommentDAOImpl();
         commentDAO.editComment(comment);
@@ -68,8 +53,6 @@ public class CommentService {
         return null;
     }
 
-    @GET
-    @Path("/hello")
     public String sayHello(){
         System.out.println("Helllooooood");
         return "hello";
